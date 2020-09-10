cfc396ecd27f0362095bd4e192868cbc0ed7101e
Cláudio Hessel <claudiorhessel@gmail.com>
2020-09-10 16:52:24 -0300

    Tratamento inicial de exceções e valições

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Clients;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
+use App\Models\ValidationClient;
 
 class ClientController extends Controller
 {
@@ -15,33 +19,68 @@ class ClientController extends Controller
     }
 
     public function getAll() {
-        $clients = $this->clientsModel->all();
-
-        return response()->json($clients);
+        try {
+            $clients = $this->clientsModel->all();
+            if($clients && count($clients) > 0) {
+                return response()->json($clients, Response::HTTP_OK);
+            } else {
+                return response()->json([], Response::HTTP_OK);
+            }
+        } catch(QueryException $e) {
+            return response()->json(['error' => 'Erro de conexão com o banco de dados'], array('status'=> Response::HTTP_INTERNAL_SERVER_ERROR));
+        }
     }
 
     public function get($id) {
-        $client = $this->clientsModel->find($id);
-
-        return response()->json($client);
+        try {
+            $client = $this->clientsModel->find($id);
+            if($client && count($client) > 0) {
+                return response()->json($client, Response::HTTP_OK);
+            } else {
+                return response()->json(null, Response::HTTP_OK);
+            }
+        } catch(QueryException $e) {
+            return response()->json(['error' => 'Erro de conexão com o banco de dados'], array('status'=> Response::HTTP_INTERNAL_SERVER_ERROR));
+        }
     }
 
     public function store(Request $request) {
-        $client = $this->clientsModel->create($request->all());
+        $validator = Validator::make(
+            $request->all(),
+            ValidationClient::RULE_CLIENT
+        );
 
-        return response()->json($client);
+        if($validator->fails()) {
+            return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+        } else {
+            try {
+                $client = $this->clientsModel->create($request->all());
+
+                return response()->json($client, Response::HTTP_CREATED);
+            } catch(QueryException $e) {
+                return response()->json(['error' => 'Erro de conexão com o banco de dados'], array('status'=> Response::HTTP_INTERNAL_SERVER_ERROR));
+            }
+        }
     }
 
     public function update($id, Request $request) {
-        $client = $this->clientsModel->find($id)
-            ->update($request->all());
+        try {
+            $client = $this->clientsModel->find($id)
+                ->update($request->all());
 
-        return response()->json($client);
+            return response()->json($client, Response::HTTP_OK);
+        } catch(QueryException $e) {
+            return response()->json(['error' => 'Erro de conexão com o banco de dados'], array('status'=> Response::HTTP_INTERNAL_SERVER_ERROR));
+        }
     }
 
     public function destroy($id) {
-        $client = $this->clientsModel->find($id)->delete();
+        try {
+            $client = $this->clientsModel->find($id)->delete();
 
-        return response()->json($client);
+            return response()->json(null, Response::HTTP_OK);
+        } catch(QueryException $e) {
+            return response()->json(['error' => 'Erro de conexão com o banco de dados'], array('status'=> Response::HTTP_INTERNAL_SERVER_ERROR));
+        }
     }
 }
---
+++ app/Models/ValidationClient.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+class ValidationClient
+{
+    const RULE_CLIENT = [
+
+        'name' => 'required|max:250',
+        'email' => 'required|email|unique:clients|max:100',
+        'phone' => 'required|max:11|integer',
+        'birth_date' => 'required|date_format:"Y-m-d"',
+        'address' => 'required|max:250',
+        'complement' => 'max:250',
+        'neighborhood' => 'required|max:50',
+        'cep' => 'required|max:8|integer'
+    ];
+}
